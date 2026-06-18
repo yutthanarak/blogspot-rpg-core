f74a2575032ead35d128de47752f26b537d7ff70
yutthanarak <yutthanarak@gmail.com>
2026-06-18 23:03:22 +0700

    Update monster-master.js

--- monster-master.js
+++ monster-master.js
@@ -1,5 +1,5 @@
 // นำเข้าฟังก์ชันจัดการ Realtime Database ของ Firebase v9
-import { ref, push, set, onValue, remove } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+
 
 // ส่งออกฟังก์ชันหลักเพื่อให้หน้า Blogspot เรียกใช้งาน
 export function initMonsterMaster(db) {
